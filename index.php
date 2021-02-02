9b80a3458760560ce5b64a13a007de5f9ee07f88
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-02-02 15:21:04 -0300

    adicionando produto a venda

--- index.php
+++ index.php
@@ -74,13 +74,13 @@
             <div class="card-body d-flex flex-column align-items-start">
               <strong class="d-inline-block mb-2 text-primary">World</strong>
               <h3 class="mb-0">
-                <a class="text-dark" href="#">Featured post</a>
+                <a class="text-dark" href="#">Camiseta</a>
               </h3>
-              <div class="mb-1 text-muted">Nov 12</div>
-              <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#">Continue reading</a>
+              <div class="mb-1 text-muted">R$ 45,00</div>
+              <p class="card-text mb-auto">Camiseta do torne-se um programador</p>
+              <a href="https://pag.ae/7WRb3vSuu" target="_blank">Comprar</a>
             </div>
-            <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
+            <img class="card-img-right flex-auto d-none d-md-block" data-src="/camisa-brasil-treino.jpg" alt="Card image cap">
           </div>
         </div>
         <div class="col-md-6">
