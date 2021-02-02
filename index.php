1dd4f290f5eee9b01ae6929b674b4b248968087d
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-02-02 14:26:26 -0300

    atualizando dados do site

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <h5 style="color: blue">Teste Caique</h5>
 <h3 style="color: blue">testet dsds </h3>
 
-<div style="display:flex; height: 400px; width:100%">
-  <div style="background-color: blue;padding: 30px;text-align:center">
+<div style="display:flex;height: 400px;width:100%;color: #8c8c8c;font-size: 30px;">
+  <div style="background-color: blue;padding: 30px;text-align:center;width: 50%;">
     Esta é a coluna da cor azul
   </div>
-  <div style="background-color: yellow;padding: 30px;text-align:center">
+  <div style="background-color: yellow;padding: 30px;text-align:center;width: 50%;">
     Esta é a coluna da cor amarelo
   </div>
 </div>
