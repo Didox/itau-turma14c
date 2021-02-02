f671fce6cb518520fa4747afb7450a56ce6c2478
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-02-02 15:29:52 -0300

    adicionando produto a venda

--- index.php
+++ index.php
@@ -72,7 +72,7 @@
         <div class="col-md-6">
           <div class="card flex-md-row mb-4 box-shadow h-md-250">
             <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-primary">World</strong>
+              <strong class="d-inline-block mb-2 text-primary">Camiseta</strong>
               <h3 class="mb-0">
                 <a class="text-dark" href="#">Camiseta</a>
               </h3>
@@ -96,15 +96,25 @@
         <div class="col-md-6">
           <div class="card flex-md-row mb-4 box-shadow h-md-250">
             <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-success">Design</strong>
+              <strong class="d-inline-block mb-2 text-primary">Caneca</strong>
               <h3 class="mb-0">
-                <a class="text-dark" href="#">Post title</a>
+                <a class="text-dark" href="#">Caneca</a>
               </h3>
-              <div class="mb-1 text-muted">Nov 11</div>
-              <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#">Continue reading</a>
+              <div class="mb-1 text-muted">R$ 35,00</div>
+              <p class="card-text mb-auto">Caneca do torne-se um programador</p>
+              <a href="https://pag.ae/7WRb3vSuu" target="_blank" style="
+                font-size: 30px;
+                background: #7bef7b;
+                display: block;
+                padding: 10px;
+                border-radius: 15px;
+                margin-bottom: 20px;
+              ">Comprar</a>
+              <a href="https://api.whatsapp.com/send?phone=551199999999999&text=Oi%20deu%20problema%20na%20minha%20compra" target="_blank" style="
+                  color: green;
+              ">Entre em contato em caso de problemas</a>
             </div>
-            <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
+            <img class="card-img-right flex-auto d-none d-md-block" src="/caneca-itau-personalizada-presentes-personalizados.jpg" alt="Card image cap">
           </div>
         </div>
       </div>
