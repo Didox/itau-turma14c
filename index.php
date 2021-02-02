3b35bf1b47969be3de05c06f1dc113d90491b7f7
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-02-02 13:56:22 -0300

    atualizando dados do site

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 <h5 style="color: blue">Fazendo uma mudança com a turma</h5>
 <h5 style="color: blue">Teste Paulo</h5>
 <h5 style="color: blue">Teste Caique</h5>
-
-
 <h3 style="color: blue">testet dsds </h3>
+
+<div style="display:flex; height: 400px">
+  <div style="background-color: blue">
+    Esta é a coluna da cor azul
+  </div>
+  <div style="background-color: yellow">
+    Esta é a coluna da cor amarelo
+  </div>
+</div>
